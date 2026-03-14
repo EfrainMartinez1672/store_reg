1e1f2b348df480352377baa3faf67c0837874a94
Efrain Martinez <hlaluz59@gmail.com>
2026-03-13 22:15:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import valuer as df
 from definition import name
 
-name()
+# Calculating the sum of the 'price' attribute/column
 total = sum(df.price)
-print("--Daily sales summary--")
+
+print(f"--Daily sales summary for {name}--")
+
+# Iterating through the 'update' attribute/column
 for dc in df.update:
     print(dc)
-print("valuer total: ", total) 
+
+print("valuer total: ", total)
